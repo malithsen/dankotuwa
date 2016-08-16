51ed04597cf14560308b5339fab4ce268b8a9b84
Malith Senaweera <malithsenaweera@gmail.com>
2016-08-16 15:04:42 +0530

    Modifiy database schema

--- backend/db.sql
+++ backend/db.sql
@@ -1,55 +1,55 @@
-BEGIN TRANSACTION;
-CREATE TABLE `order` (
-  `OrderNumber` VARCHAR(30) DEFAULT NULL,
-  `Date&Time` DATETIME DEFAULT NULL,
-PRIMARY KEY (`OrderNumber`)
-);
 CREATE TABLE `sales_representative` (
-  `EmployeeID` VARCHAR(30) NOT NULL,
+  `EmployeeID` bigint(30) NOT NULL AUTO_INCREMENT,
   `Name` VARCHAR(30) DEFAULT NULL,
   `UserName` VARCHAR(30) DEFAULT NULL,
-  `Password` VARCHAR(30) DEFAULT NULL,
+  `Email` VARCHAR(30) DEFAULT NULL,
   PRIMARY KEY (`EmployeeID`)
 );
+
 CREATE TABLE `product` (
-  `ProductID` VARCHAR(30) NOT NULL,
+  `ProductID` bigint(30) NOT NULL AUTO_INCREMENT,
   `ProductName` VARCHAR(30) DEFAULT NULL,
   `Price` VARCHAR(30) DEFAULT NULL,
   PRIMARY KEY (`ProductID`)
 );
+
+CREATE TABLE `category` (
+  `CategoryID` bigint(30) NOT NULL AUTO_INCREMENT,
+  `CategoryName` VARCHAR(30) DEFAULT NULL,
+  PRIMARY KEY (`CategoryID`)
+);
+
+CREATE TABLE `order_info` (
+  `OrderNumber` bigint(30) NOT NULL AUTO_INCREMENT,
+  `Epoch` bigint(10) DEFAULT NULL,
+  PRIMARY KEY (`OrderNumber`)
+);
+
+CREATE TABLE `order_product` (
+  `OrderNumber` bigint(30) NOT NULL,
+  `ProductID` bigint(30) NOT NULL,
+  `CategoryID` bigint(30) NOT NULL,
+  PRIMARY KEY (`OrderNumber`, `ProductID`),
+  CONSTRAINT `fk_product` FOREIGN KEY (`ProductID`) REFERENCES `product` (`ProductID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_category` FOREIGN KEY (`CategoryID`) REFERENCES `category` (`CategoryID`) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
 CREATE TABLE `dealer` (
-  `DealerID` VARCHAR(30) NOT NULL,
+  `DealerID` bigint(30) NOT NULL AUTO_INCREMENT,
   `Name` VARCHAR(30) DEFAULT NULL,
-  `Address` VARCHAR(100) DEFAULT NULL,
+  `Geo_lat` VARCHAR(9) DEFAULT NULL,
+  `Geo_lng` VARCHAR(9) DEFAULT NULL,
   PRIMARY KEY (`DealerID`)
 );
-CREATE TABLE `ordering` (
-  `EmployeeID` VARCHAR(30) NOT NULL,
-  `OrderNumber` VARCHAR(30) DEFAULT NULL,
-  `DealerID` VARCHAR(30) DEFAULT NULL,
-  `Location` VARCHAR(30) DEFAULT NULL,
+
+CREATE TABLE `dealer_rep_order` (
+  `EmployeeID` bigint(30) NOT NULL,
+  `OrderNumber` bigint(30) NOT NULL,
+  `DealerID` bigint(30) NOT NULL,
+  `Correct_location` int(1) DEFAULT NULL,
   PRIMARY KEY (`EmployeeID`,`OrderNumber`,`DealerID`),
-CONSTRAINT `fk_emp` FOREIGN KEY (`EmployeeID`)
-REFERENCES `sales_representative` (`EmployeeID`)
-ON DELETE CASCADE ON UPDATE CASCADE,
-CONSTRAINT `fk_oID` FOREIGN KEY (`OrderNumber`)
-REFERENCES `order` (`OrderNumber`)
-ON DELETE CASCADE ON UPDATE CASCADE,
-CONSTRAINT `fk_dealer` FOREIGN KEY (`DealerID`)
-REFERENCES `dealer` (`DealerID`)
-ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `fk_emp` FOREIGN KEY (`EmployeeID`) REFERENCES `sales_representative` (`EmployeeID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_oID` FOREIGN KEY (`OrderNumber`) REFERENCES `order_info` (`OrderNumber`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_dealer` FOREIGN KEY (`DealerID`) REFERENCES `dealer` (`DealerID`) ON DELETE CASCADE ON UPDATE CASCADE
 );
-CREATE TABLE `order_product ` (
-  `OrderNumber` VARCHAR(30) NOT NULL,
-  `ProductID` VARCHAR(30) DEFAULT NULL,
-  PRIMARY KEY (`OrderNumber`,`ProductID`),
-CONSTRAINT `fk_oID2` FOREIGN KEY (`OrderNumber`)
-REFERENCES `order ` (`OrderNumber`)
-ON DELETE CASCADE ON UPDATE CASCADE,
-CONSTRAINT `fk_product` FOREIGN KEY (`ProductID`)
-REFERENCES ` product` (`ProductID`)
-ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-
-COMMIT;
