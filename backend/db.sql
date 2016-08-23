93f564cd9cd4e40a05d9a98bf16eac0b268a2fc8
Malith Senaweera <malithsenaweera@gmail.com>
2016-08-23 21:30:06 +0530

    Add dummy data to db

--- backend/db.sql
+++ backend/db.sql
@@ -1,56 +1,213 @@
-CREATE TABLE `sales_representative` (
-  `EmployeeID` bigint(30) NOT NULL AUTO_INCREMENT,
-  `Name` VARCHAR(30) DEFAULT NULL,
-  `UserName` VARCHAR(30) DEFAULT NULL,
-  `Email` VARCHAR(30) DEFAULT NULL,
-  PRIMARY KEY (`EmployeeID`)
-);
+-- MySQL dump 10.16  Distrib 10.1.14-MariaDB, for Linux (i686)
+--
+-- Host: localhost    Database: dankotuwa
+-- ------------------------------------------------------
+-- Server version	10.1.14-MariaDB
 
-CREATE TABLE `product` (
-  `ProductID` bigint(30) NOT NULL AUTO_INCREMENT,
-  `ProductName` VARCHAR(30) DEFAULT NULL,
-  `Price` VARCHAR(30) DEFAULT NULL,
-  PRIMARY KEY (`ProductID`)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `category` (
   `CategoryID` bigint(30) NOT NULL AUTO_INCREMENT,
-  `CategoryName` VARCHAR(30) DEFAULT NULL,
+  `CategoryName` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`CategoryID`)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE `order_info` (
-  `OrderNumber` bigint(30) NOT NULL AUTO_INCREMENT,
-  `Epoch` bigint(10) DEFAULT NULL,
-  PRIMARY KEY (`OrderNumber`)
-);
+--
+-- Dumping data for table `category`
+--
 
-CREATE TABLE `order_product` (
-  `OrderNumber` bigint(30) NOT NULL,
-  `ProductID` bigint(30) NOT NULL,
-  `CategoryID` bigint(30) NOT NULL,
-  `Quantity` bigint(30) NOT NULL,
-  PRIMARY KEY (`OrderNumber`, `ProductID`, `CategoryID`),
-  CONSTRAINT `fk_product` FOREIGN KEY (`ProductID`) REFERENCES `product` (`ProductID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_category` FOREIGN KEY (`CategoryID`) REFERENCES `category` (`CategoryID`) ON DELETE CASCADE ON UPDATE CASCADE
-);
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'Blue rose'),(2,'Livy'),(3,'Lasting rose');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `dealer`
+--
 
+DROP TABLE IF EXISTS `dealer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `dealer` (
   `DealerID` bigint(30) NOT NULL AUTO_INCREMENT,
-  `Name` VARCHAR(30) DEFAULT NULL,
-  `Geo_lat` VARCHAR(9) DEFAULT NULL,
-  `Geo_lng` VARCHAR(9) DEFAULT NULL,
+  `Name` varchar(30) DEFAULT NULL,
+  `Geo_lat` varchar(9) DEFAULT NULL,
+  `Geo_lng` varchar(9) DEFAULT NULL,
   PRIMARY KEY (`DealerID`)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `dealer`
+--
+
+LOCK TABLES `dealer` WRITE;
+/*!40000 ALTER TABLE `dealer` DISABLE KEYS */;
+INSERT INTO `dealer` VALUES (1,'Sameera','6.9297169','6.9297169'),(2,'Nimantha','6.8906424','79.876862');
+/*!40000 ALTER TABLE `dealer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dealer_rep_order`
+--
+
+DROP TABLE IF EXISTS `dealer_rep_order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `dealer_rep_order` (
   `EmployeeID` bigint(30) NOT NULL,
   `OrderNumber` bigint(30) NOT NULL,
   `DealerID` bigint(30) NOT NULL,
   `Correct_location` int(1) DEFAULT NULL,
   PRIMARY KEY (`EmployeeID`,`OrderNumber`,`DealerID`),
+  KEY `fk_oID` (`OrderNumber`),
+  KEY `fk_dealer` (`DealerID`),
+  CONSTRAINT `fk_dealer` FOREIGN KEY (`DealerID`) REFERENCES `dealer` (`DealerID`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_emp` FOREIGN KEY (`EmployeeID`) REFERENCES `sales_representative` (`EmployeeID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_oID` FOREIGN KEY (`OrderNumber`) REFERENCES `order_info` (`OrderNumber`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_dealer` FOREIGN KEY (`DealerID`) REFERENCES `dealer` (`DealerID`) ON DELETE CASCADE ON UPDATE CASCADE
-);
+  CONSTRAINT `fk_oID` FOREIGN KEY (`OrderNumber`) REFERENCES `order_info` (`OrderNumber`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dealer_rep_order`
+--
+
+LOCK TABLES `dealer_rep_order` WRITE;
+/*!40000 ALTER TABLE `dealer_rep_order` DISABLE KEYS */;
+INSERT INTO `dealer_rep_order` VALUES (1,1,1,1),(1,3,1,1),(2,2,2,0),(2,29,1,1),(3,30,1,1);
+/*!40000 ALTER TABLE `dealer_rep_order` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order_info`
+--
+
+DROP TABLE IF EXISTS `order_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `order_info` (
+  `OrderNumber` bigint(30) NOT NULL AUTO_INCREMENT,
+  `Epoch` bigint(10) DEFAULT NULL,
+  PRIMARY KEY (`OrderNumber`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order_info`
+--
+
+LOCK TABLES `order_info` WRITE;
+/*!40000 ALTER TABLE `order_info` DISABLE KEYS */;
+INSERT INTO `order_info` VALUES (1,1471341682),(2,1471341100),(3,1471341699),(29,1471451040),(30,1471939714);
+/*!40000 ALTER TABLE `order_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order_product`
+--
+
+DROP TABLE IF EXISTS `order_product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `order_product` (
+  `OrderNumber` bigint(30) NOT NULL,
+  `ProductID` bigint(30) NOT NULL,
+  `CategoryID` bigint(30) NOT NULL,
+  `Quantity` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`OrderNumber`,`ProductID`,`CategoryID`),
+  KEY `fk_product` (`ProductID`),
+  KEY `fk_category` (`CategoryID`),
+  CONSTRAINT `fk_category` FOREIGN KEY (`CategoryID`) REFERENCES `category` (`CategoryID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_product` FOREIGN KEY (`ProductID`) REFERENCES `product` (`ProductID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order_product`
+--
+
+LOCK TABLES `order_product` WRITE;
+/*!40000 ALTER TABLE `order_product` DISABLE KEYS */;
+INSERT INTO `order_product` VALUES (1,1,3,2),(1,2,2,2),(2,1,1,3),(2,2,1,3),(2,3,3,3),(3,2,1,1),(3,3,3,1),(29,2,2,4),(29,2,3,3),(30,1,1,2),(30,2,2,1);
+/*!40000 ALTER TABLE `order_product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product` (
+  `ProductID` bigint(30) NOT NULL AUTO_INCREMENT,
+  `ProductName` varchar(30) DEFAULT NULL,
+  `Price` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`ProductID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (1,'Dinner plate','10'),(2,'Salad plate','15'),(3,'Cereak bowl','15');
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sales_representative`
+--
+
+DROP TABLE IF EXISTS `sales_representative`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sales_representative` (
+  `EmployeeID` bigint(30) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(30) DEFAULT NULL,
+  `UserName` varchar(30) DEFAULT NULL,
+  `Email` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`EmployeeID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sales_representative`
+--
+
+LOCK TABLES `sales_representative` WRITE;
+/*!40000 ALTER TABLE `sales_representative` DISABLE KEYS */;
+INSERT INTO `sales_representative` VALUES (1,'Udara Nanayakkara','udara','udara@dankotuwa.com'),(2,'Anuradha Perera','anup','anup@dankotuwa.com'),(3,'Malith Senaweera','malithsen','malithsenaweera@gmail.com');
+/*!40000 ALTER TABLE `sales_representative` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-08-23 21:27:30
