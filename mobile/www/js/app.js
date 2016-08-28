c6a9e7004ca809ea293364cbe6baf874e5c7f241
kavindasas <kaviliya123@gmail.com>
2016-08-28 16:53:01 +0530

    signature Added

--- mobile/www/js/app.js
+++ mobile/www/js/app.js
@@ -1,6 +1,6 @@
 // Dankotuwa
 
-angular.module('dankotuwa', ['ionic','ionic.service.core', 'ngCordova', 'auth0', 'angular-storage', 'angular-jwt','signature'])
+angular.module('dankotuwa', ['ionic','ionic.service.core', 'ngCordova', 'auth0', 'angular-storage', 'angular-jwt'])
 
 .value("BackendUrl", "http://ec2-54-244-208-146.us-west-2.compute.amazonaws.com:8080")
 
