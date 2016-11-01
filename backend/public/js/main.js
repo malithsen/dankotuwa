fc1d17fa85954985c7bb83e2e0675775b0457fa1
Malith Senaweera <malithsenaweera@gmail.com>
2016-11-01 12:07:50 +0530

    Change nav-content color. Change backend path

--- backend/public/js/main.js
+++ backend/public/js/main.js
@@ -1,6 +1,6 @@
 var dankotuwaApp = angular.module('dankotuwaApp', ['ui.router', 'angularMoment', 'angular-storage']);
 
-dankotuwaApp.value("BackendUrl", "http://localhost:8080");
+dankotuwaApp.value("BackendUrl", "http://ec2-54-244-208-146.us-west-2.compute.amazonaws.com:8080");
 
 dankotuwaApp.config(['$stateProvider', '$urlRouterProvider', "$locationProvider", "$httpProvider", function($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
 
