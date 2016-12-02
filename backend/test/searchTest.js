d87028febfaea600a2df4ba12c9e6d0dd02c129b
isirayapa <isirayapa@gmail.com>
2016-12-02 13:27:13 +0530

    searchTest

---
+++ backend/test/searchTest.js
@@ -0,0 +1,17 @@
+describe("Testing the search function",function(){
+	it("Sample data test1",function(){
+		browser.get("http://localhost:8080/");
+		element(by.model("search")).sendKeys("MR. MARSUK");
+		//Note - The test have been done based on dummy data To run for the real data it has to be modified.
+		expect(element.all(by.css('.body-col')).count()).toEqual(4);
+		element.all(by.css('.body-col')).count().then(function(count){
+			console.log("Search - data1 test success.\nSearch count: ",count);
+		})
+		// var count = element.all(by.css('.body-col'));
+		// count.then(function(result){
+		// 	expect(result.length).toEqual(4);
+		// });
+		
+	});
+
+})
