6669415b209edad3d9d14b2c667d4cc84b3ddfbb
isirayapa <isirayapa@gmail.com>
2016-12-02 13:40:50 +0530

    searchTest added dataset2

--- backend/test/searchTest.js
+++ backend/test/searchTest.js
@@ -13,5 +13,14 @@ describe("Testing the search function",function(){
 		// });
 		
 	});
+	it("Sample data test2",function(){
+		browser.get("http://localhost:8080/");
+		element(by.model("search")).sendKeys("MR.RANJAN");
+		//Note - The test have been done based on dummy data To run for the real data it has to be modified.
+		expect(element.all(by.css('.body-col')).count()).toEqual(1);
+		element.all(by.css('.body-col')).count().then(function(count){
+			console.log("Search - data2 test success.\nSearch count: ",count);
+		})		
+	});
 
 })
