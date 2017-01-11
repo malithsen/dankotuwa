385b095edf88831b48a2b10c4106999d7f2f586c
Malith Senaweera <malithsenaweera@gmail.com>
2017-01-11 18:19:42 +0530

    Unit test update routes and signature retrieval

--- backend/test/test.js
+++ backend/test/test.js
@@ -3,8 +3,6 @@ var should = require("should");
 var server = supertest.agent("http://localhost:8080");
 var app = require('../app').app.listen(8080);
 
-// UNIT test begin
-
 describe("API endpoints :",function(){
 
   after(function (done) {
@@ -12,11 +10,10 @@ describe("API endpoints :",function(){
     done();
   });
 
-  it("/api/products should return 401 with invalid token error",function(done){
+  it("GET /api/products should return 401 with invalid token error",function(done){
 
     server
     .get('/api/products')
-    // .expect("Content-type",/json/)
     .expect(401)
     .end(function(err,res){
       res.status.should.equal(401);
@@ -25,11 +22,10 @@ describe("API endpoints :",function(){
     });
   });
 
-  it("/api/category should return 401 with invalid token error",function(done){
+  it("GET /api/category should return 401 with invalid token error",function(done){
 
     server
     .get('/api/categories')
-    // .expect("Content-type",/json/)
     .expect(401)
     .end(function(err,res){
       res.status.should.equal(401);
@@ -38,7 +34,7 @@ describe("API endpoints :",function(){
     });
   });
 
-  it("/api/rep should return reps list",function(done){
+  it("GET /api/rep should return reps list",function(done){
 
     server
     .get('/api/reps')
@@ -52,7 +48,7 @@ describe("API endpoints :",function(){
     });
   });
 
-  it("/api/orders/rep/:id should return orders by rep id",function(done){
+  it("GET /api/orders/rep/:id should return orders by rep id",function(done){
 
     server
     .get('/api/orders/rep/1')
@@ -63,12 +59,24 @@ describe("API endpoints :",function(){
       res.body.should.have.property('1').which.is.a.Object();
       res.body['1'].should.have.property('epoch').which.is.a.Number();
       res.body['1'].should.have.property('items').which.is.a.Array();
-      // res.body[0].should.have.property('UserName');
       done();
     });
   });
 
-  it('/api/order should post order', function(done) {
+  it("GET /api/order/signature/:orderid should return dealer's signature",function(done){
+
+    server
+    .get('/api/order/signature/1')
+    .expect("Content-type",/json/)
+    .expect(200)
+    .end(function(err,res){
+      res.status.should.equal(200);
+      res.body.should.have.property('sign').which.is.a.String();
+      done();
+    });
+  });
+
+  it('POST /api/order should post order', function(done) {
     server
       .post('/api/order')
       .send({"epoch":1472210342,"repID":3,"dealerID":1,"atLocation":1,"items":[{"product":{"ProductID":2,"ProductName":"Salad plate"},"category":{"CategoryID":2,"CategoryName":"Livy"},"quantity":2}]})
@@ -78,6 +86,50 @@ describe("API endpoints :",function(){
         if (err) done(err);
         res.text.should.be.equal('OK');
         //res.body.participant.should.have.property('nuid', '98ASDF988SDF89SDF89989SDF9898');
+        done();
+      });
+  });
+  it('POST /api/dealer without token should return an error', function(done) {
+    server
+      .post('/api/dealer')
+      .send({"name":'test user',"lat":12312312,"lng":12312312,"phone":123456789,"address":"Fake address", "city": "Fake city"})
+      .expect(401)
+      .end(function(err, res) {
+        if (err) done(err);
+        res.text.should.be.equal('Invalid token');
+        done();
+      });
+  });
+  it('PUT /api/update/reps should update sales rep table', function(done) {
+    server
+      .put('/api/update/reps')
+      .send({"data":"Fake name,fakeusername, fake@email.com"})
+      .expect(200)
+      .end(function(err, res) {
+        if (err) done(err);
+        res.text.should.be.equal('OK');
+        done();
+      });
+  });
+  it('PUT /api/update/products should update the product', function(done) {
+    server
+      .put('/api/update/products')
+      .send({"data": "0,test product, 0"})
+      .expect(200)
+      .end(function(err, res) {
+        if (err) done(err);
+        res.text.should.be.equal('OK');
+        done();
+      });
+  });
+  it('PUT /api/update/category should update the category', function(done) {
+    server
+      .put('/api/update/categories')
+      .send({"data": "0,test category, 0"})
+      .expect(200)
+      .end(function(err, res) {
+        if (err) done(err);
+        res.text.should.be.equal('OK');
         done();
       });
   });
